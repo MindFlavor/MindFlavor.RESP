91768ad2e9649787c7d23d91eb198b38d82fc0e0
Francesco Cogno <francesco.cogno@outlook.com>
2014-11-13 19:35:34 +0100

    Added SQLCLR and scripts

--- MindFlavor.RESP.SQLCLR/TSQL.Scripts/test.sql
+++ MindFlavor.RESP.SQLCLR/TSQL.Scripts/test.sql
@@ -4,10 +4,9 @@ GO
 DECLARE @txt NVARCHAR(MAX);
 WHILE 1=1
 BEGIN
-	SET @txt = N'sample message from SPID ' + CONVERT(NVARCHAR, @@SPID) + '.';
-	EXEC Redis.Publish 
-		--'10.50.50.1', 
-		'gitlab.pelucchi.local',
+	SET @txt = N'sample message from SPID ' + CONVERT(NVARCHAR, @@SPID) + '. I am ' + USER_NAME() + ' at local time is ' + CONVERT(NVARCHAR, GETDATE());
+	EXEC Redis.Publish  
+		'ubuntu-spare.pelucchi.local',
 		6379, N'channel', @txt;
 
 	WAITFOR DELAY '00:00:01';
